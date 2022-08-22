da6f2d41f16a6a1b58f27159d451bc5f016dd0b8
MinimiseOCE <jackson.whiteau@gmail.com>
2022-08-22 17:53:23 +0800

    Add queue format

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ import {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
+  EmbedBuilder,
 } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -22,8 +23,58 @@ client.on("interactionCreate", async (interaction) => {
 
   const { commandName } = interaction;
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
+  if (interaction.commandName === "ping") {
+    let players = [];
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("join")
+        .setLabel("Join Queue")
+        .setStyle(ButtonStyle.Success),
+      new ButtonBuilder()
+        .setCustomId("leave")
+        .setLabel("Leave Queue")
+        .setStyle(ButtonStyle.Danger)
+    );
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle("Valorant 10 Man")
+      .setAuthor({
+        name: "OCE MM BOT",
+        iconURL:
+          "https://pbs.twimg.com/profile_images/1234434416346198016/qiKPqdwP_400x400.jpg",
+      })
+      .setThumbnail(
+        "https://pbs.twimg.com/profile_images/1234434416346198016/qiKPqdwP_400x400.jpg"
+      )
+      .addFields({ name: "Players", value: "Queue up noobs" })
+      .addFields(
+        players.map((player) => ({
+          inline: true,
+          name: `${player}`,
+        }))
+      )
+      .setTimestamp()
+      .setFooter({
+        text: "Made by Minimise",
+        iconURL: "https://i.imgur.com/AfFp7pu.png",
+      });
+
+    client.on("interactionCreate", (interaction) => {
+      if (!interaction.isButton()) return;
+      console.log(interaction);
+    });
+
+    await interaction.reply({
+      ephemeral: false,
+      embeds: [exampleEmbed],
+      components: [row],
+    });
+
+    const filter = (btnInt) => {
+      return true;
+    };
+
+    const collector = channel.createMessageComponentCollector;
   } else if (commandName === "server") {
     await interaction.reply(
       `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
