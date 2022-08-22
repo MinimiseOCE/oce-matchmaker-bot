429974e9f1b3da76e1420ba392b884a8da44188a
MinimiseOCE <jackson.whiteau@gmail.com>
2022-08-22 16:11:03 +0800

    Install test commands

--- bot.js
+++ bot.js
@@ -1,6 +1,13 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require("discord.js");
-require("dotenv").config();
+import {
+  Client,
+  GatewayIntentBits,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -8,6 +15,24 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
   console.log("Ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+    );
+  } else if (commandName === "user") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+    );
+  }
 });
 
 // Login to Discord with your client's token
